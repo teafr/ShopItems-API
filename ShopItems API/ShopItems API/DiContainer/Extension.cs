dd70e53222e3b342dae44ee73746680c06f97e8a
teafr <tookyoumy7@gmail.com>
2025-03-20 12:03:48 +0200

    Moved Program lodic to extencion methods

---
+++ ShopItems API/ShopItems API/DiContainer/Extension.cs
@@ -0,0 +1,37 @@
+﻿using DataLibrary;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ShopItems_API.DiContainer
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services.AddSingleton<IDataAccess, DataAccess>();
+
+            return services;
+        }
+        public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+                };
+            });
+
+            return services;
+        }
+    }
+}
--- ShopItems API/ShopItems API/Program.cs
+++ ShopItems API/ShopItems API/Program.cs
@@ -1,34 +1,19 @@
 using DataLibrary;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using ShopItems_API.DiContainer;
 using System.Text;
 
 namespace ShopItems_API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
-            builder.Services.AddSingleton<IDataAccess, DataAccess>();
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-                };
-            });
+            builder.Services.AddAuthenticationServices(builder).AddDependencies();
 
             var app = builder.Build();
 
